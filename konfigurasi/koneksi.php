64cd9221d1183f13fde4d91e4882ae359c92ab67
delana <delana@gmail.com>
2020-08-13 21:41:32 +0700

    menambahkan koneksi.php

---
+++ konfigurasi/koneksi.php
@@ -0,0 +1,8 @@
+<?php
+    $server = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "ramadan";
+
+    $conn = mysqli_connect($server,$username,$password,$dbname) or die("database tidak terhubung");
+?>
