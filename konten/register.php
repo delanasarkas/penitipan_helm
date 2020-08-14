eff6a60515572ae05eafd92183a24c3a33d5723f
ramadan017 <ramadansatria017@gmail.com>
2020-08-14 07:35:43 -0700

    membuat halaman register.php

---
+++ konten/register.php
@@ -0,0 +1,46 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Register </title>
+    <link rel="stylesheet" href="../assets/bootstrap.css">
+</head>
+<body class="bg-primary">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-6 offset-lg-3">
+                <div class="card mt-3 bg-info">
+                    <div class="card-body">
+                        <h1 class="text-center">REGISTER</h1>
+                        <img src="../image/logohero.png" class="img-fluid" alt="Responsive image">
+                        <p class="text-muted text-center">
+                            &copy; Aplikasi Penitipan Barang 2020
+                        </p>
+                        <hr>
+                        <form>
+                            <div class="form-group">
+                                <label for="exampleFormControlInput1">Nik</label>
+                                <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Username...">
+                            </div>
+                            <div class="form-group">
+                                <label for="exampleFormControlInput1">Username</label>
+                                <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Username...">
+                            </div>
+                            <div class="form-group">
+                                <label for="exampleFormControlInput1">Password</label>
+                                <input type="password" class="form-control" id="exampleFormControlInput1" placeholder="Password...">
+                            </div>
+                            <button type="button" class="btn btn-success btn-block">Register</button>
+
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
